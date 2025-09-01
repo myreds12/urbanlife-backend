e476860589e94171be5d9e675322756b649226ed
Alif_04 <alifnaufalh@gmail.com>
2025-09-01 09:06:17 +0700

    feat(AuthService): remove password hashing from register method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -38,12 +38,9 @@ export class AuthService {
 
   async register(data: RegisterDto) {
     console.log(data, 'DATA REGISTER');
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-
     return this.usersService.create(
       {
         ...data,
-        password: hashedPassword,
       },
       undefined,
     );
